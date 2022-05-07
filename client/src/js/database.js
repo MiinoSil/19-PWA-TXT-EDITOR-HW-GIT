80bffd6bded5e7267c326a6797434682b3ced0e0
MiinoSil <andvngo@gmail.com>
2022-05-07 12:45:24 -0700

    edited database js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,43 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+  console.log('PUT content in the database');
+  const jateDb = await openDB('jate', 1);
+  const txt = jdateDb.transaction('jdate', 'ReadWrite');
+  const store = txt.objectStore('jate');
+  const request = store.put({
+    id: 1,
+    value: content
+  });
+  const result = await request;
+  console.log('Content saved to the database, result.value');
+} catch (err) {
+    console.log(err);
+    console.error('putDb not implemented');
+  };
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    console.log('GET content from the database');
+    const jateDb = await openDB('jate', 1);
+    const txt = jateDb.transaction('jate', 'ReadOnly');
+    const store = txt.objectStore('jate');
+    const request = store.get(1);
+    const result = await request;
+    if (result) {
+      console.log('Content retrieved from database', result.value)
+    } else {
+      console.log('No content found in the database');
+    }
+  } catch(err) {
+      console.log(err);
+      console.error('getDb not implemented');
+    };
+};
 
 initdb();
